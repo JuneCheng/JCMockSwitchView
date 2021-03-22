2b54d99ac4e661896e12d66c4135fc6354403eec
JuneCheng <755038283@qq.com>
2021-03-22 14:32:30 +0800

    Initial Commit

---
+++ JCMockSwitchView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JCMockSwitchView
+//
+//  Created by JuneCheng on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JCMockSwitchView/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  JCMockSwitchView
+//
+//  Created by JuneCheng on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
